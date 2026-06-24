f4948dff57181291a6d4aed0113148fc5532b957
Sergey <sergey@example.com>
2026-06-24 20:42:42 +0300

    Fix MSX integration and remove duplicate TVXPlugin load

--- streamlume_server.js
+++ streamlume_server.js
@@ -11,6 +11,7 @@ const { runAutoblog } = require('./autoblog');
 const { runYouTubeBot } = require('./youtube_bot');
 
 const app = express();
+app.set('trust proxy', true);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
